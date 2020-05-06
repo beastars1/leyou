44adc839d719ff35a2dca53e41f1dd35d827a5f6
Beasters <1229435236@qq.com>
2020-05-06 13:45:09 +0800

    添加查询接口

--- leyou-item/leyou-item-interface/src/main/java/com/leyou/item/api/BrandApi.java
+++ leyou-item/leyou-item-interface/src/main/java/com/leyou/item/api/BrandApi.java
@@ -4,9 +4,15 @@ import com.leyou.item.pojo.Brand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface BrandApi {
 
     @GetMapping("brand/{bid}")
     Brand queryBrandByBid(@PathVariable("bid")Long bid);
+
+    @GetMapping("brand")
+    List<Brand> queryBrandByIds(@RequestParam("ids")List<Long> ids);
 }
